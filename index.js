88f55e3dc20f0de40e0c46ac2156b9635c7f1880
Ituski Yamanaka <149759057+ituki1216@users.noreply.github.com>
2024-10-27 21:30:02 +0900

    Update index.js

--- index.js
+++ index.js
@@ -5,3 +5,7 @@ const port = 3000;
 
 app.use(express.json()); // JSONリクエストのパース
 app.use(express.urlencoded({ extended: true })); 
+
+app.get('/', (req, res) => {
+    res.send('<h1>ホームページ</h1><p>ここがトップページです。</p>');
+});
