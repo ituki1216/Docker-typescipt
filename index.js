4fa324db5c82b48a830af4f531036e5967be64d7
Ituski Yamanaka <149759057+ituki1216@users.noreply.github.com>
2024-10-27 21:30:14 +0900

    Update index.js

--- index.js
+++ index.js
@@ -9,3 +9,8 @@ app.use(express.urlencoded({ extended: true }));
 app.get('/', (req, res) => {
     res.send('<h1>ホームページ</h1><p>ここがトップページです。</p>');
 });
+
+
+app.get('/about', (req, res) => {
+    res.send('<h1>About Us</h1><p>このアプリについての情報です。</p>');
+});
