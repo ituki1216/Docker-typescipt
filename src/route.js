cba48763e2db7c1c6fdc91c56f805313fc348694
Ituski Yamanaka <149759057+ituki1216@users.noreply.github.com>
2024-10-28 16:56:41 +0900

    Update route.js

--- src/route.js
+++ src/route.js
@@ -1,3 +1,8 @@
 app.use(express.json()); // JSONリクエストのパース
 app.use(express.urlencoded({ extended: true })); 
 
+
+app.get('/', (req, res) => {
+    res.send('<h1>ホームページ</h1><p>ここがトップページです。</p>');
+});
+
