3e6be7411a5d702c3129af3ffdb01785345003b6
Ituski Yamanaka <149759057+ituki1216@users.noreply.github.com>
2024-10-28 16:47:56 +0900

    Update index.js

--- index.js
+++ index.js
@@ -3,22 +3,3 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.use(express.json()); // JSONリクエストのパース
-app.use(express.urlencoded({ extended: true })); 
-
-app.get('/', (req, res) => {
-    res.send('<h1>ホームページ</h1><p>ここがトップページです。</p>');
-});
-
-
-app.get('/about', (req, res) => {
-    res.send('<h1>About Us</h1><p>このアプリについての情報です。</p>');
-});
-
-app.use((req, res) => {
-    res.status(404).send('<h1>404 Not Found</h1><p>お探しのページは見つかりませんでした。</p>');
-});
-
-app.listen(port, () => {
-    console.log(`サーバーは http://localhost:${port} で動作中`);
-});
