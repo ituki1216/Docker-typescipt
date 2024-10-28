d652ad31102b8ffef5bd3cf05c698680423c72f6
Ituski Yamanaka <149759057+ituki1216@users.noreply.github.com>
2024-10-28 16:56:48 +0900

    Update route.js

--- src/route.js
+++ src/route.js
@@ -6,3 +6,7 @@ app.get('/', (req, res) => {
     res.send('<h1>ホームページ</h1><p>ここがトップページです。</p>');
 });
 
+app.get('/about', (req, res) => {
+    res.send('<h1>About Us</h1><p>このアプリについての情報です。</p>');
+});
+
